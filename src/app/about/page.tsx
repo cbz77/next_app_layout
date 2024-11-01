228ba2ab078e819e638ed800354de0880361a0a8
gerek <gerek@ebm.cz>
2024-11-01 11:00:24 +0100

    about - api - datatable

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,24 @@
+'use client'
 import React from "react";
 
 const AboutPage: React.FC = () => {
-    return <div>About ... </div>;
+
+    const apiUrl = "https://datausa.io/api/data?drilldowns=Nation&measures=Population";
+
+    const fetchData = async () => {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    };
+
+    return (
+        <div>
+
+            <h2>About - api test</h2>
+
+            <button className="btn bg-blue-500 p-2 rounded hover:bg-blue-600 shadow" onClick={fetchData}>Fetch Data</button>
+        </div>
+    );
 };
 
 export default AboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
-
+'use client';
+import React, { useEffect, useState } from "react";
 export default function Home() {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="home">
     
