845d3541c79d72a07d4c6e76cadb497bbaec8197
gerek <gerek@ebm.cz>
2024-11-01 12:53:11 +0100

    about page - data table - fetch

--- src/app/about/datatable.tsx
+++ src/app/about/datatable.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-const DataTable = () => {
+import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+
+interface DataTableProps {
+    data: any;
+}
+
+const DataTable : React.FC<DataTableProps> = ({ data }) : React.ReactElement => {
+
+    const [propData, setPropData] = useState<any[]>([]);
+
+    useEffect(() => {
+        if(data && data.data)
+        setPropData(data.data);
+    }, [data]);
+
+    console.log("data table component data", propData);
+  
+     // Column Definitions: Defines the columns to be displayed.
+     const colDefs = [
+        { headerName: "Year", field: "Year" },
+        { headerName: "Population", field: "Population" },
+        { headerName: "Nation", field: "Nation" },
+    ];
+
     return (
-        <div>
-            
+        <div className="mt-4">
+
+            <div className="ag-theme-quartz-dark" style={{ height: 500 }}>
+                <AgGridReact
+                    rowData={propData}
+                    columnDefs={colDefs}
+                />
+            </div>
+
         </div>
     );
 };  
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 import React from "react";
+import DataTable from "./datatable";
+import { useEffect, useState } from "react";
 
 const AboutPage: React.FC = () => {
 
     const apiUrl = "https://datausa.io/api/data?drilldowns=Nation&measures=Population";
 
+    const [fetchedRowData, setFetchedRowData] = useState<object>({});
+
     const fetchData = async () => {
+        
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        setFetchedRowData(data);
     };
 
     return (
@@ -17,6 +22,9 @@ const AboutPage: React.FC = () => {
             <h2>About - api test</h2>
 
             <button className="btn bg-blue-500 p-2 rounded hover:bg-blue-600 shadow" onClick={fetchData}>Fetch Data</button>
+
+            <DataTable data={fetchedRowData}></DataTable>
+
         </div>
     );
 };
