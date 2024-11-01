35d0f8796bdfa04c1cec3fd18683eb784316b986
gerek <gerek@ebm.cz>
2024-11-01 13:30:07 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Ensures Next.js uses static export mode
+  images: {
+    unoptimized: true, // Disables image optimization (required for static exports)
+  },
+  basePath: '/next_app_layout', // Replace 'project-name' with your repository name
+  assetPrefix: '/next_app_layout', // Ensures assets are loaded from the correct path
 };
 
 export default nextConfig;
