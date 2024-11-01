8da7759eed1403da83a4f593eaf487eb67396d9a
gerek <gerek@ebm.cz>
2024-11-01 14:01:32 +0100

    url adresy fix

--- src/components/url_adresy.tsx
+++ src/components/url_adresy.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+const base_path = "/next_app_layout";
+
 const url_adresy = {
-    home: "/",
-    about: "/about",
-    services: "/services",
-    contacts: "/contacts"
+    home: `${base_path}/`,
+    about: `${base_path}/about`,
+    services: `${base_path}/services`,
+    contacts: `${base_path}/contacts`
 }
 
 export default url_adresy
